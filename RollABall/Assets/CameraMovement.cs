17c41f0e11748bea5c605e1d232274e2c92e31d6
Akpil <jyk0802@naver.com>
2020-07-25 16:55:51 +0900

    Move camera, set pickup basic

---
+++ RollABall/Assets/CameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject Player;
+    private Vector3 mOffset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        mOffset = transform.position - Player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Player.transform.position + mOffset;
+    }
+}

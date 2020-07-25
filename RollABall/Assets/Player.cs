6faf513ebd718e054b5b11a6d3136a393ed4efab
Akpil <jyk0802@naver.com>
2020-07-25 15:51:18 +0900

    Roll a ball setup

---
+++ RollABall/Assets/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float Speed;
+    private Rigidbody RB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start");
+        RB = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("update");
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 dir = new Vector3(horizontal, 0, vertical);
+
+        RB.velocity = dir * Speed;
+        //RB.AddForce(dir * Speed);
+        //transform.Translate(dir * Time.deltaTime * Speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
